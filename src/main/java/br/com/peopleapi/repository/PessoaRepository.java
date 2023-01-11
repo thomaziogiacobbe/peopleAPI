6dfd53cc4ffa3b29238a2f347fad523b0aa8f40a
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-01-11 17:11:48 -0300

    created repository for Pessoa

---
+++ src/main/java/br/com/peopleapi/repository/PessoaRepository.java
@@ -0,0 +1,14 @@
+package br.com.peopleapi.repository;
+
+import br.com.peopleapi.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    Optional<Pessoa> findById(Long id);
+
+    List<Pessoa> findAllByNomeContainsIgnoreCase(String nome);
+}
