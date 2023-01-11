9b74669623f4ac99cad15620ab3600f164613424
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-01-11 18:46:10 -0300

    create PessoaController

---
+++ src/main/java/br/com/peopleapi/controller/PessoaController.java
@@ -0,0 +1,52 @@
+package br.com.peopleapi.controller;
+
+import br.com.peopleapi.model.Pessoa;
+import br.com.peopleapi.service.PessoaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+
+    private final PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Pessoa> newPessoa(@RequestBody Pessoa pessoa) {
+        try {
+            Pessoa p = pessoaService.createPessoa(pessoa);
+            return new ResponseEntity<>(p, HttpStatus.OK);
+        } catch (DataIntegrityViolationException ex) {
+            return new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        }
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<Pessoa> editPessoa(@PathVariable("id") Long id,
+                                             @RequestBody Pessoa pessoa) {
+        return pessoaService.editPessoa(id, pessoa)
+                .map(p -> new ResponseEntity<>(p, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(null, HttpStatus.BAD_REQUEST));
+    }
+
+    @GetMapping("/get/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Pessoa getPessoa(@PathVariable("id") Long id) {
+        return pessoaService.findPessoa(id);
+    }
+
+    @GetMapping("/getAll")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Pessoa> getAllPessoas() {
+        return pessoaService.getAllPessoas();
+    }
+}
