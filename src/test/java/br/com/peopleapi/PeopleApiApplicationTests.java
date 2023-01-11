565031e41e112be05cdf34a68d480228f8167602
thomazio <thomazio@mertins.com.br>
2023-01-11 15:23:30 -0300

    application creation

---
+++ src/test/java/br/com/peopleapi/PeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
