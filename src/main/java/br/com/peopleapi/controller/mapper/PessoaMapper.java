1366ffc27f10536cd568536bed46f145e7e29a89
thomaziogiacobbe <tgiacobbe@outlook.com>
2023-01-17 00:07:55 -0300

    changes to pessoa mapper

--- src/main/java/br/com/peopleapi/controller/mapper/PessoaMapper.java
+++ src/main/java/br/com/peopleapi/controller/mapper/PessoaMapper.java
@@ -18,8 +18,8 @@ public class PessoaMapper {
                 .id(pessoaDTO.id())
                 .dataNascimento(LocalDate.parse(pessoaDTO.dataNascimento(), DateTimeFormatter.ofPattern("dd/MM/yyyy")))
                 .nome(pessoaDTO.nome())
-                .enderecos( pessoaDTO.enderecos() != null ?
-                            pessoaDTO.enderecos().stream().map(PessoaMapper::toEnderecoEntity).toList()
+                .enderecos(pessoaDTO.enderecos() != null ?
+                            pessoaDTO.enderecos().stream().map(EnderecoMapper::toEntity).toList()
                         : null)
                 .build();
     }
@@ -31,36 +31,7 @@ public class PessoaMapper {
                 .dataNascimento(pessoa.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")))
                 .nome(pessoa.getNome())
                 .enderecos(pessoa.getEnderecos() != null ?
-                        pessoa.getEnderecos().stream().map(PessoaMapper::toEnderecoDTO).toList()
-                        : null)
-                .build();
-    }
-
-    private static Endereco toEnderecoEntity(EnderecoDTO endereco) {
-        return Endereco
-                .builder()
-                .id(endereco.id())
-                .cep(endereco.cep())
-                .cidade(endereco.cidade())
-                .logradouro(endereco.logradouro())
-                .isPrincipal(endereco.isPrincipal())
-                .pessoa(endereco.pessoa() != null ?
-                        Pessoa.builder().id(endereco.pessoa().id()).build()
-                        : null)
-                .build();
-    }
-
-    private static EnderecoDTO toEnderecoDTO(Endereco endereco) {
-        return EnderecoDTO
-                .builder()
-                .id(endereco.getId())
-                .cep(endereco.getCep())
-                .numero(endereco.getNumero())
-                .cidade(endereco.getCidade())
-                .logradouro(endereco.getLogradouro())
-                .isPrincipal(endereco.getIsPrincipal())
-                .pessoa(endereco.getPessoa() != null ?
-                        PessoaDTO.builder().id(endereco.getPessoa().getId()).build()
+                        pessoa.getEnderecos().stream().map(EnderecoMapper::toDTO).toList()
                         : null)
                 .build();
     }
